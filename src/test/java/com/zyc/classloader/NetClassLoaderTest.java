a0aa13b5ca28289123064b3594c2958d851483f5
zhouych <zhouych329@163.com>
2018-09-04 17:41:43 +0800

    ...

--- src/test/java/com/zyc/classloader/NetClassLoaderTest.java
+++ src/test/java/com/zyc/classloader/NetClassLoaderTest.java
@@ -6,9 +6,9 @@ public class NetClassLoaderTest {
 
 	@Test
 	public void test() throws Exception {
-		String url = "";
+		String url = "https://github.com/zhouych/classloader/tree/master/src/main/java";
 		NetClassLoader loader = new NetClassLoader(url);
-		Class<?> clazz = loader.loadClass("");
+		Class<?> clazz = loader.loadClass("com.zyc.classloader.tmps.User");
 		System.out.println(clazz.newInstance().toString());
 	}
 	
